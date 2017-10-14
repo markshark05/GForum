4692497145b5c5eb25f82c51e828985645ce5e25
Mark <mark.shark05@hotmail.com>
2017-10-14 17:23:56 +0300

    Add admin layout.

---
+++ GForum/GForum.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace GForum.Web.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "Admin";
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "Admin_default",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ GForum/GForum.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace GForum.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
