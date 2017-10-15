3d253899d873c3b4621af10aaec2ccc6d9d50cce
Mark <mark.shark05@hotmail.com>
2017-10-15 16:19:46 +0300

    Add posts viewing in administartion.

---
+++ GForum/GForum.Web/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Mvc;
+using AutoMapper;
+using GForum.Services.Contracts;
+using GForum.Web.Areas.Admin.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GForum.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PostsController : Controller
+    {
+        private readonly IPostService postService;
+        private readonly IMapper mapper;
+
+        public PostsController(
+            IPostService postService,
+            IMapper mapper)
+        {
+            this.postService = postService;
+            this.mapper = mapper;
+        }
+
+        // GET: /Admin/Categories
+        public ActionResult Index()
+        {
+            var model = new AdminPostsViewModel
+            {
+                PostsQueriable = this.postService.GetAll(true),
+            };
+
+            return View(model);
+        }
+
+        //// GET: /Admin/Categories/Delete
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(Guid id)
+        //{
+        //    this.postService.Delete(id);
+
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ GForum/GForum.Web/Areas/Admin/Models/AdminPostsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using GForum.Data.Models;
+
+namespace GForum.Web.Areas.Admin.Models
+{
+    public class AdminPostsViewModel
+    {
+        public IQueryable<Post> PostsQueriable { get; set; }
+    }
+}
