3fe149a689782de8f1d789627f1b8a38e7086109
Mark <mark.shark05@hotmail.com>
2017-10-15 19:19:48 +0300

    Add category service tests.

--- GForum/GForum.Services.Tests/CategoryServiceTests.cs
+++ GForum/GForum.Services.Tests/CategoryServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using GForum.Data.Contracts;
 using GForum.Data.Models;
 using Moq;
@@ -27,6 +28,47 @@ namespace GForum.Services.Tests
                 c.AuthorId == "userid" &&
                 c.Title == "title" &&
                 c.Id != default(Guid))), Times.Once);
+        }
+
+        [Test]
+        public void Delete_ShouldCallRepositoryDeleteForAllPostsAndUnitOFWorkComplete()
+        {
+            // Arrange
+            var posts = new[] { new Post(), new Post(), new Post() };
+            var category = new Category { Id = Guid.NewGuid(), Posts = posts };
+            var categories = new[] { category }.AsQueryable();
+
+            var repositoryMock = new Mock<IRepository<Category>>();
+            repositoryMock.Setup(x => x.QueryAll).Returns(categories);
+
+            var postRepositoryMock = new Mock<IRepository<Post>>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var postService = new CategoryService(unitOfWorkMock.Object, repositoryMock.Object, postRepositoryMock.Object);
+
+            // Act
+            postService.Delete(category.Id);
+
+            // Assert
+            postRepositoryMock.Verify(x => x.Remove(It.IsAny<Post>()), Times.Exactly(posts.Length));
+            unitOfWorkMock.Verify(x => x.Complete(), Times.Once);
+        }
+
+        [Test]
+        public void Delete_ShouldNotComplete_IfNoCategoriesAreFound()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IRepository<Category>>();
+            var postRepositoryMock = new Mock<IRepository<Post>>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var postService = new CategoryService(unitOfWorkMock.Object, repositoryMock.Object, postRepositoryMock.Object);
+
+            // Act
+            postService.Delete(Guid.Empty);
+
+            // Assert
+            unitOfWorkMock.Verify(x => x.Complete(), Times.Never);
         }
     }
 }
