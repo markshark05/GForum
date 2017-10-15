b0de34a390f7fca0774a9234e849114fa7c11166
Mark <mark.shark05@hotmail.com>
2017-10-15 23:50:09 +0300

    Add postscontroller tests.

--- GForum/GForum.Web.Tests/Controllers/PostsControllerTests.cs
+++ GForum/GForum.Web.Tests/Controllers/PostsControllerTests.cs
@@ -6,6 +6,8 @@ using GForum.Data.Models;
 using GForum.Services.Contracts;
 using GForum.Web.Contracts.Identity;
 using GForum.Web.Controllers;
+using GForum.Web.Models.Comments;
+using GForum.Web.Models.Posts;
 using Moq;
 using NUnit.Framework;
 
@@ -41,6 +43,41 @@ namespace GForum.Web.Tests.Controllers
         }
 
         [Test]
+        public void SubmitGet_ShouldCallCategoryServiceForCategoryAndReturnView()
+        {
+            // Arrange
+            var postServiceMock = new Mock<IPostService>();
+            var voteServiceMock = new Mock<IVoteService>();
+            var commentServiceMock = new Mock<ICommentService>();
+            var userManagerMock = new Mock<IApplicationUserManager>();
+
+            var categoryServiceMock = new Mock<ICategoryService>();
+            categoryServiceMock
+                .Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<bool>()))
+                .Returns(new[] { new Category() }.AsQueryable());
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(x => x.Map<PostSubmitViewModel>(It.IsAny<object>()))
+                .Returns(new PostSubmitViewModel());
+
+            var controller = new PostsController(
+                categoryServiceMock.Object,
+                postServiceMock.Object,
+                voteServiceMock.Object,
+                commentServiceMock.Object,
+                userManagerMock.Object,
+                mapperMock.Object);
+
+            // Act
+            var result = controller.Submit(Guid.Empty);
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            categoryServiceMock.Verify(x => x.GetById(It.IsAny<Guid>(), It.IsAny<bool>()), Times.Once);
+        }
+
+        [Test]
         public void SubmitGet_ShouldReturnNotFound_WhenCategoryIsNull()
         {
             // Arrange
@@ -64,6 +101,33 @@ namespace GForum.Web.Tests.Controllers
 
             // Assert
             Assert.IsInstanceOf<HttpNotFoundResult>(result);
+        }
+
+        [Test]
+        public void Comment_ShouldCallPostServiceGetById()
+        {
+            // Arrange
+            var categoryServiceMock = new Mock<ICategoryService>();
+            var postServiceMock = new Mock<IPostService>();
+            var voteServiceMock = new Mock<IVoteService>();
+            var commentServiceMock = new Mock<ICommentService>();
+            var userManagerMock = new Mock<IApplicationUserManager>();
+            var mapperMock = new Mock<IMapper>();
+
+            var controller = new PostsController(
+                categoryServiceMock.Object,
+                postServiceMock.Object,
+                voteServiceMock.Object,
+                commentServiceMock.Object,
+                userManagerMock.Object,
+                mapperMock.Object);
+            var model = new CommentAddViewModel();
+
+            // Act
+            var result = controller.Comment(model);
+
+            // Assert
+            postServiceMock.Verify(x => x.GetById(It.IsAny<Guid>(), It.IsAny<bool>()), Times.Once);
         }
     }
 }
